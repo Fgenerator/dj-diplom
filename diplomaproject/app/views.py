29f7e014f16c198f7f05387b879c112ff5954239
Evgeny Ermakov <desteev@gmail.com>
2020-09-30 19:41:49 +0500

    Added basic views

--- diplomaproject/app/views.py
+++ diplomaproject/app/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    template = 'index.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def phone_view(request):
+    template = 'phone.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def smartphone_view(request):
+    template = 'smartphones.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def login_view(request):
+    template = 'login.html'
+    context = {}
+
+    return render(request, template, context)
+
+
+def cart_view(request):
+    template = 'cart.html'
+    context = {}
+
+    return render(request, template, context)
