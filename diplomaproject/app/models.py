e8d439dd983d754b4c97ce54fc9522285b21ed54
Evgeny Ermakov <desteev@gmail.com>
2020-10-04 20:02:48 +0500

    Edited all models

--- diplomaproject/app/models.py
+++ diplomaproject/app/models.py
@@ -2,28 +2,45 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.IntegerField()
-    description = models.TextField(max_length=500)
-    image = models.FileField(upload_to='products/%Y/%m/%d/')
+    category = models.ForeignKey('Category',
+                                 related_name='products',
+                                 default=None,
+                                 on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, default=None)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
 
-class Reviews(models.Model):
-    name = models.CharField(max_length=50)
-    text = models.TextField(max_length=500)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
-    products = models.ManyToManyField(
-        Product,
-        through='ProductCategoryInfo',
-        default=None,
-    )
+    name = models.CharField(max_length=200,
+                            db_index=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True, default=None)
     is_root = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
-class ProductCategoryInfo(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=50)
+    text = models.TextField(max_length=500)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
