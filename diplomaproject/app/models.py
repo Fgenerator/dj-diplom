c27bea105febc8de46c350c8cd03c17c05f1e822
Evgeny Ermakov <desteev@gmail.com>
2020-10-05 21:19:26 +0500

    Edited Review model

--- diplomaproject/app/models.py
+++ diplomaproject/app/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     category = models.ForeignKey('Category',
-                                 related_name='products',
+                                 related_name='media',
                                  default=None,
                                  on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True, default=None)
-    image = models.ImageField(upload_to='products/%Y/%m/%d',
+    image = models.ImageField(upload_to='products',
                               blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -40,7 +40,10 @@ class Category(models.Model):
 
 class Review(models.Model):
     name = models.CharField(max_length=50)
+    header = models.CharField(max_length=100)
     text = models.TextField(max_length=500)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
+    products = models.ManyToManyField(Product,
+                                      related_name='media',
+                                      default=None)
